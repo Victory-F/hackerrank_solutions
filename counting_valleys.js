f3f5645050279f2ee93ed8ea031d6a224890a672
Viktoriia <100996452+Victory-F@users.noreply.github.com>
2023-03-30 16:32:49 +0200

    Create counting_valleys.js

---
+++ counting_valleys.js
@@ -0,0 +1,17 @@
+function countingValleys(steps, path) {
+  let stepsCount = 0;
+  let valleysCount = 0;
+
+  for (let i = 0; i < path.length; i++) {
+    if (path[i] === "U") {
+      stepsCount++;
+      if (stepsCount === 0) {
+        valleysCount++;
+      }
+    } else {
+      stepsCount--;
+    }
+  }
+
+  return valleysCount;
+}
