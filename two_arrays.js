be48e94dd7165ac74d1fa734c9d29ec89446be03
Viktoriia <100996452+Victory-F@users.noreply.github.com>
2023-04-05 22:10:31 +0200

    Create two_arrays.js

---
+++ two_arrays.js
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/two-arrays/problem
+
+function twoArrays(k, A, B) {
+    A.sort((a, b) => a - b);
+    B.sort((a, b) => b - a);
+    for (let i = 0; i < A.length; i++) {
+        if (A[i] + B[i] < k) {
+            return "NO";
+        }
+    }
+    return "YES";
+}
