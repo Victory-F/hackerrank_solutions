2e21ad05b20805f49d72c3a0875e796004d2dbba
Viktoriia <100996452+Victory-F@users.noreply.github.com>
2023-04-17 11:22:45 +0200

    Create mars_exploration.js

---
+++ mars_exploration.js
@@ -0,0 +1,11 @@
+// https://www.hackerrank.com/challenges/mars-exploration/problem
+
+function marsExploration(s) {
+    let count = 0;
+    for (let i = 0; i < s.length; i += 3) {
+        if (s[i] !== 'S') count++;
+        if (s[i + 1] !== 'O') count++;
+        if (s[i + 2] !== 'S') count++;
+    }
+    return count;
+}
